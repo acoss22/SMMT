76db3365cac64e850410147c10324293c422024f
acoss22 <anacseq@gmail.com>
2023-08-15 00:51:24 +0100

    fix: tab behavior switching add

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
 import React, { useState } from 'react';
-import './styles.scss'; // Import the SCSS file
+import './styles.scss';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header';
 import FollowerCount from './components/Tabs/FollowerCount/FollowerCount';
 import Tab from './components/Tabs/Tab';
 import Tasks from './components/Tabs/Tasks/DailyTasks';
 
-
 const App: React.FC = () => {
   const tabs = ['Followers', 'Tasks'];
   const [activeTab, setActiveTab] = useState(0);
-  const [postTweetDaily, setPostTweetDaily] = useState(false);
 
   const handleTabClick = (index: number) => {
     setActiveTab(index);
   };
 
+  const [postTweetDaily, setPostTweetDaily] = useState(false); // State for Tasks component
+
   return (
     <div>
       <Header title="Social Media Management Tool" />
-      <div className="main-container">
-        <Tab tabs={tabs} activeTab={activeTab} handleTabClick={handleTabClick}>
-          <div>
-            <FollowerCount />
-          </div>
-          <div>
+      <Tab tabs={tabs} activeTab={activeTab} handleTabClick={handleTabClick}>
+        <div>
+          {activeTab === 0 && <FollowerCount />}
+          {activeTab === 1 && (
             <Tasks postTweetDaily={postTweetDaily} setPostTweetDaily={setPostTweetDaily} />
-          </div>
-        </Tab>
-      </div>
+          )}
+        </div>
+      </Tab>
       <Footer copyright="2023 Ana Sequeira" />
     </div>
   );

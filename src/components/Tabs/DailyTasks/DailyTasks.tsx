0f49fc9b900805887757338aa6d4558c30717d8d
acoss22 <anacseq@gmail.com>
2023-08-16 11:12:54 +0100

    styles: add styles for tabs

--- src/components/Tabs/DailyTasks/DailyTasks.tsx
+++ src/components/Tabs/DailyTasks/DailyTasks.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styles from './dailytasks.module.scss';
+import React from "react";
+import styles from "./dailytasks.module.scss";
 
 interface Task {
   id: string;
@@ -14,19 +14,21 @@ interface TasksProps {
 
 const Tasks: React.FC<TasksProps> = ({ tasks }) => {
   return (
-    <div className={styles['tab2-content']}>
+    <div className={styles["tab2-content"]}>
       <h2>Tasks</h2>
-      {tasks.map(task => (
-        <label key={task.id} className={styles['task-checkbox-label']}>
-          <input
-            type="checkbox"
-            className={styles['task-checkbox']}
-            checked={task.state}
-            onChange={e => task.setState(e.target.checked)}
-          />
-          {task.description}
-        </label>
-      ))}
+      <div className={styles["checkbox-list-content"]}>
+        {tasks.map((task) => (
+          <label key={task.id} className={styles["task-checkbox-label"]}>
+            <input
+              type="checkbox"
+              className={styles["task-checkbox"]}
+              checked={task.state}
+              onChange={(e) => task.setState(e.target.checked)}
+            />
+            {task.description}
+          </label>
+        ))}
+      </div>
     </div>
   );
 };

4300c35219c40beedf7601aea040b3da4b4570eb
acoss22 <anacseq@gmail.com>
2023-08-16 17:32:29 +0100

    feat:  activity logs

--- src/components/Tabs/ActivityLogs/ActivityLogs.tsx
+++ src/components/Tabs/ActivityLogs/ActivityLogs.tsx
@@ -1,23 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../store/store';
+import styles from './activitylogs.module.scss';
 
 const ActivityLogs: React.FC = () => {
   const followers = useSelector((state: RootState) => state.followers);
-  const lastUpdated = useSelector((state: RootState) => state.lastUpdated);
   const followerHistory = useSelector((state: RootState) => state.followerHistory);
-
   const [previousCounts, setPreviousCounts] = useState<{ [platform: string]: number }>({});
 
   useEffect(() => {
-    // Initialize previous follower counts when component mounts
     setPreviousCounts(followers);
   }, [followers]);
 
   const generateLogMessages = () => {
     const logMessages: string[] = [];
 
-    // Iterate through follower history and generate log messages
     followerHistory.forEach(({ platform, count, timestamp }) => {
       const prevCount = previousCounts[platform] || 0;
       if (prevCount !== count) {
@@ -29,13 +26,20 @@ const ActivityLogs: React.FC = () => {
     return logMessages;
   };
 
+  const logMessages = generateLogMessages(); // Declare logMessages variable here
+
   return (
     <div>
       <h2>Activity Logs</h2>
       <div>
-        {generateLogMessages().map((logMessage, index) => (
-          <p key={index}>{logMessage}</p>
-        ))}
+        {logMessages.length > 0 ? (
+          logMessages.map((logMessage, index) => (
+            <p key={index}>{logMessage}</p>
+          ))
+        ) : (
+          <><p className={styles.logs}>There is no activity logs yet. </p>
+          <p className={styles.logs}> Update a follower count to see some activity here.</p></>
+        )}
       </div>
     </div>
   );

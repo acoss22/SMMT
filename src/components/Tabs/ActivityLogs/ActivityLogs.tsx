ea498358bf72dd73e19d5c1fe93cab26ca14b06c
acoss22 <anacseq@gmail.com>
2023-08-17 17:14:44 +0100

    style: responsive add

--- src/components/Tabs/ActivityLogs/ActivityLogs.tsx
+++ src/components/Tabs/ActivityLogs/ActivityLogs.tsx
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../../store/store';
-import styles from './activitylogs.module.scss';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store/store";
+import styles from "./activitylogs.module.scss";
 
 const ActivityLogs: React.FC = () => {
   const followers = useSelector((state: RootState) => state.followers);
-  const followerHistory = useSelector((state: RootState) => state.followerHistory);
-  const [previousCounts, setPreviousCounts] = useState<{ [platform: string]: number }>({});
+  const followerHistory = useSelector(
+    (state: RootState) => state.followerHistory
+  );
+  const [previousCounts, setPreviousCounts] = useState<{
+    [platform: string]: number;
+  }>({});
 
   useEffect(() => {
     setPreviousCounts(followers);
@@ -18,7 +22,9 @@ const ActivityLogs: React.FC = () => {
     followerHistory.forEach(({ platform, count, timestamp }) => {
       const prevCount = previousCounts[platform] || 0;
       if (prevCount !== count) {
-        const logMessage = `Platform ${platform} went from ${count} followers to ${count - (count - prevCount)} followers at ${timestamp}`;
+        const logMessage = `Platform ${platform} went from ${count} followers to ${
+          count - (count - prevCount)
+        } followers at ${timestamp}`;
         logMessages.push(logMessage);
       }
     });
@@ -26,19 +32,26 @@ const ActivityLogs: React.FC = () => {
     return logMessages;
   };
 
-  const logMessages = generateLogMessages(); // Declare logMessages variable here
+  const logMessages = generateLogMessages();
 
   return (
-    <div>
+    <div className={styles["tab2-content"]}>
       <h2>Activity Logs</h2>
-      <div>
+      <div className={styles['checkbox-list-content']}>
         {logMessages.length > 0 ? (
           logMessages.map((logMessage, index) => (
+            <div className={styles['list-item']}>
             <p key={index}>{logMessage}</p>
+            </div>
           ))
         ) : (
-          <><p className={styles.logs}>There is no activity logs yet. </p>
-          <p className={styles.logs}> Update a follower count to see some activity here.</p></>
+          <>
+            <p className={styles.logs}>There is no activity logs yet. </p>
+            <p className={styles.logs}>
+              {" "}
+              Update a follower count to see some activity here.
+            </p>
+          </>
         )}
       </div>
     </div>

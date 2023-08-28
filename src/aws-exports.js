94babd5bb35e39c331d1002600c003d0d105d56d
Ana Sequeira <33404890+acoss22@users.noreply.github.com>
2023-08-28 14:45:15 +0100

    Update aws-exports.js

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,9 +1,9 @@
 const awsconfig = {
-    aws_project_region: "us-east-1",
-    aws_cognito_identity_pool_id: "us-east-1:us-east-1_JDRkJzrpM", // Update this line
-    aws_cognito_region: "us-east-1",
-    aws_user_pools_id: "us-east-1_JDRkJzrpM", // Update this line
-    aws_user_pools_web_client_id: "1cd1d2mk241jlrfvi938072v28",
+    aws_project_region: "xxxxxxx",
+    aws_cognito_identity_pool_id: "xxxxx", // Update this line
+    aws_cognito_region: "xxxxxx1",
+    aws_user_pools_id: "zzzzz", // Update this line
+    aws_user_pools_web_client_id: "1xxxxxxv28",
     oauth: {},
   };
   

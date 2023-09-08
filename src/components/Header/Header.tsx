8a78f861f9db6743e88d65cdaaba688ddc0ebd7f
acoss22 <anacseq@gmail.com>
2023-09-08 15:58:06 +0100

    feat: add hello message to current logged in user in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
-import styles from './header.module.scss';
+import React from "react";
+import styles from "./header.module.scss";
 
 interface HeaderProps {
   title: string;
   isLoggedIn: boolean;
   onLogout: () => void;
+  username: string | null;
 }
 
-const Header: React.FC<HeaderProps> = ({ title, isLoggedIn, onLogout }) => {
+const Header: React.FC<HeaderProps> = ({
+  title,
+  isLoggedIn,
+  onLogout,
+  username,
+}) => {
   return (
     <header className={styles.header}>
       <h1>{title}</h1>
       {isLoggedIn && (
-        <button className={styles.logoutButton} onClick={onLogout}>
-          Logout
-        </button>
+        <>
+          <div className="userActionsContainer">
+            <span>Hello {username}</span>
+          </div>
+          <button className={styles.logoutButton} onClick={onLogout}>
+            Logout
+          </button>
+        </>
       )}
     </header>
   );
